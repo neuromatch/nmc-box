caf5a835ebdef24acd69f38bc7b6e57799de8d0d
tulakann <tulakan.r@gmail.com>
2021-08-23 16:30:18 +0700

    refactor(faq): add md link support to componentize

--- frontend/src/pages/faq.js
+++ frontend/src/pages/faq.js
@@ -7,14 +7,25 @@ import Layout from "../components/layout"
 import { color } from "../utils"
 
 // -- CONSTANT
+// TODO: move to utils
 const regex = {
-  src: "https://regexr.com/2rj36",
-  pattern: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+~#?&//=]*)?/gi,
+  url: {
+    src: "https://regexr.com/2rj36",
+    pattern: /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+~#?&//=]*)?/gi,
+  },
+  mdLink: {
+    src: 'https://regexr.com/3ciio',
+    pattern: /(\[((?:\[[^\]]*\]|[^\[\]])*)\]\([ \t]*()<?((?:\([^)]*\)|[^()\s])*?)>?[ \t]*((['"])(.*?)\6[ \t]*)?\))/gi,
+  }
 }
 
 // -- FUNCTION
+const composeRegex = (...regexes) => new RegExp(regexes.map(regex => regex.source).join("|"), 'g')
+
 const componentize = text => {
-  const parsed = text.match(regex.pattern)
+  const regs = composeRegex(regex.url.pattern, regex.mdLink.pattern)
+  // [matchObject] -> if there are 3 and 5 -> markdown link
+  const parsed = [...text.matchAll(regs)]
 
   if (!parsed || parsed?.length === 0) {
     return text
@@ -22,22 +33,31 @@ const componentize = text => {
 
   const result = []
   let remainingText = text
-  // let lastIndex = 0
 
   parsed.forEach((p, ind) => {
-    const start = remainingText.indexOf(p)
-    const end = start + p.length
+    const p0 = p[0]
+
+    const start = remainingText.indexOf(p0)
+    const end = start + p0.length
 
     result.push(remainingText.slice(0, start))
 
-    if (p.includes("@")) {
-      result.push(<code key={p + ind}>{p}</code>)
-    } else {
+    if (p[3] && p[5]) {
       result.push(
-        <a key={p + ind} href={p} target="_blank" rel="noopener noreferrer">
-          {p}
+        <a key={p[3] + p[5] + ind} href={p[5]} target="_blank" rel="noopener noreferrer">
+          {p[3]}
         </a>
       )
+    } else {
+      if (p0.includes("@")) {
+        result.push(<code key={p0 + ind}>{p0}</code>)
+      } else {
+        result.push(
+          <a key={p0 + ind} href={p0} target="_blank" rel="noopener noreferrer">
+            {p0}
+          </a>
+        )
+      }
     }
 
     remainingText = remainingText.slice(end)
