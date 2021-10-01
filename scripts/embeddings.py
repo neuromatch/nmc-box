fbeb480584920a0e1c262b1ff840d327227d8778
titipata <my.titipat@gmail.com>
2021-10-01 17:30:54 +0700

    Using es_config to create embeddings instead of paths

--- scripts/embeddings.py
+++ scripts/embeddings.py
@@ -15,12 +15,15 @@ Options:
 import os
 import os.path as op
 import json
+import yaml
 from glob import glob
 from docopt import docopt
+from dotenv import load_dotenv
 
 import joblib
 import numpy as np
 import pandas as pd
+from pyairtable import Table
 from tqdm.auto import tqdm
 from transformers import AutoTokenizer, AutoModel
 
@@ -28,7 +31,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.neighbors import NearestNeighbors
 
+from es_index import read_submissions, keys_airtable
+
+load_dotenv(dotenv_path="../.env")  # setting all credentials here
 MAX_BATCH_SIZE = 16
+assert os.environ.get(
+    "AIRTABLE_KEY"
+), "Please check if AIRTABLE_KEY is specified in environment file"
+airtable_key = os.environ.get("AIRTABLE_KEY")
 
 
 def chunks(lst, chunk_size=MAX_BATCH_SIZE):
@@ -116,35 +126,47 @@ if __name__ == "__main__":
     # create embeddings
     if not op.exists(save_path):
         os.makedirs(save_path)
-    paths = glob(op.join("..", "sitedata", "agenda", "*.csv")) + glob(
-        op.join("..", "sitedata", "agenda", "*.json")
-    )
+
+    with open("es_config.yml") as f:
+        es_config = yaml.load(f, Loader=yaml.FullLoader)
 
     # option if not specified, set option as `sent_embed`
     option = arguments.get("--option")
     if option is None:
-        option = "sent_embed"
+        option = "lsa"
 
-    for path in tqdm(paths):
-        print(f"Calculate embeddings for {path}\n")
-        basename = op.basename(path).split(".")[0]
-
+    for k, v in tqdm(es_config["editions"].items()):
+        print(f"Calculate embeddings for edition {k}\n")
+        basename = f"agenda-{k}"
+        path = v.get("path", "")
         if path.lower().endswith(".json"):
             df = pd.read_json(path).fillna("")
         elif path.lower().endswith(".csv"):
             df = pd.read_csv(path).fillna("")
+        elif v.get("airtable_id") is not None:
+            # if filter_accepted, only filter accepted submissions
+            filter_accepted = v.get("filter_accepted", False)
+            submissions = Table(airtable_key, v["airtable_id"], v["table_name"]).all()
+            submissions = read_submissions(
+                submissions, keys=keys_airtable, filter_accepted=filter_accepted
+            )
+            df = pd.DataFrame(submissions).fillna("")
         else:
             df = pd.read_csv(path).fillna("")
 
         # calculate embeddings, save in JSON with the same basename
-        paper_embeddings = calculate_embeddings(df, option=option)
-        json.dump(
-            paper_embeddings,
-            open(op.join(save_path, basename + ".json"), "w"),
-            indent=2,
-        )
+        if len(df) > 0:
+            paper_embeddings = calculate_embeddings(df, option=option)
+            json.dump(
+                paper_embeddings,
+                open(op.join(save_path, basename + ".json"), "w"),
+                indent=2,
+            )
 
-        # nearest neighbors, save in joblib with the same basename
-        X = np.vstack([p["embedding"] for p in paper_embeddings])
-        nbrs_model = NearestNeighbors(n_neighbors=len(X)).fit(X)
-        joblib.dump(nbrs_model, op.join(save_path, basename + ".joblib"))
+            # nearest neighbors, save in joblib with the same basename
+            X = np.vstack([p["embedding"] for p in paper_embeddings])
+            nbrs_model = NearestNeighbors(n_neighbors=len(X)).fit(X)
+            joblib.dump(nbrs_model, op.join(save_path, basename + ".joblib"))
+            print(f"Saved embeddings and nearest neighbor model for edition {k}")
+        else:
+            print("Length of dataframe is 0, please recheck the data")
