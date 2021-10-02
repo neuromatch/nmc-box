561287d6113dea260dab6b7ed6817bc08c0713e1
tulakann <tulakan.r@gmail.com>
2021-10-02 23:36:59 +0700

    fix(gatsby-browser): fix some styles not load on refresh in production

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { CookiesProvider  } from 'react-cookie';
-import { ThemeProvider } from './src/styles/themeContext';
+import React from "react"
+import ReactDOM from "react-dom"
+import { CookiesProvider } from "react-cookie"
+import { ThemeProvider } from "./src/styles/themeContext"
 
 export const wrapRootElement = ({ element }) => (
   <CookiesProvider>
-    <ThemeProvider>
-      {element}
-    </ThemeProvider>
+    <ThemeProvider>{element}</ThemeProvider>
   </CookiesProvider>
-);
+)
+
+// this is a hack to fix missing styles on refresh in production
+// reference: https://github.com/gatsbyjs/gatsby/issues/17676#issuecomment-535796737
+export const replaceHydrateFunction = () => {
+  return (element, container, callback) => {
+    ReactDOM.render(element, container, callback)
+  }
+}
