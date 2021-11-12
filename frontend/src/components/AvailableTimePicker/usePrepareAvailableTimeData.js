b6ed25f096760de38049e8d7d4d477c2ed8d50a3
tulakann <tulakan.r@gmail.com>
2021-11-12 11:39:48 +0700

    fix(usePrepareAvailableTimeData): fix missing field to be queried

--- frontend/src/components/AvailableTimePicker/usePrepareAvailableTimeData.js
+++ frontend/src/components/AvailableTimePicker/usePrepareAvailableTimeData.js
@@ -56,6 +56,7 @@ function usePrepareAvailableTimeData() {
             current_edition
             main_timezone
             editions {
+              edition
               main_conference {
                 start
                 end
@@ -76,6 +77,7 @@ function usePrepareAvailableTimeData() {
     // data of all edition
     editions,
   } = sitedata
+
   // get data of current edition
   // for AvailableTimePicker we need start and end dates of the main conference
   const {
